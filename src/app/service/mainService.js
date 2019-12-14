314b62c68a5fc9c8f19e501f38c8c2da5f842b48
leonzw <leon.wei.zhao@gmail.com>
2019-12-14 18:19:36 +0800

    chatjs got

--- src/app/service/mainService.js
+++ src/app/service/mainService.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = require(path.join(app.getAppPath(), "src","config.json"))
 
 module.exports.openChatWindow = () => {
-    let chatWindow = new BrowserWindow({ width: 800, height: 600 })
+    let chatWindow = new BrowserWindow({ width: 1280, height: 800 })
     chatWindow.loadFile('src/resources/chat.html')
     if (config.openDebugTool) chatWindow.webContents.openDevTools()
     chatWindow.on('close', ()=>{
