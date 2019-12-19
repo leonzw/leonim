b86848807be5a266fc6dc19d40fdf1b4482ebad1
leonzw <leon.wei.zhao@gmail.com>
2019-12-19 18:31:33 +0800

    clean code

--- src/app/main.js
+++ src/app/main.js
@@ -9,44 +9,21 @@ let mainService  = require(path.join(app.getAppPath(), "src", "app", "service", 
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', ()=>{
-  mainService.createMainWindow()
-  mainService.setTrayIcon()
-  // 然后加载应用的 index.html。
-  mainService.getWin().loadFile(path.join(app.getAppPath(), 'src', 'resources', 'html', 'index.html'))
+    mainService.createMainWindow()
+    mainService.setTrayIcon()
+    // 然后加载应用的 index.html。
+    mainService.getWin().loadFile(path.join(app.getAppPath(), 'src', 'resources', 'html', 'index.html'))
 })
 
 
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。如果需要保持mac上关闭按钮不退出，打开下面的代码
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }else{
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。如果需要保持mac上关闭按钮不退出，打开下面的代码
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }else{
 
-  }
+    }
 })
-
-
-
-
-// 剩下主进程代码。
-// 启动定时任务线程
-//const {spawn} = require("child_process");
-//var child_cron = spawn("node",["app/libs/cron.js"],{ stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
-
-
-
-// ipcMain.on('stopCronResult', (event, arg) => {
-//     console.log(arg) // prints "ping"
-//     event.returnValue = 'pong'
-// })
-
-/*
-exports.getWinObj = () => {
-  return win;
-}
-
-exports.yanganSqlite = require(path.join(app.getAppPath(), "app","libs","sqlite.js"));
-*/
